d110f614edd5e032c0412f74c99c0ac6ad8f0ad8
Yasmin243 <yasminbarone468@gmail.com>
2024-06-20 08:15:56 -0300

    carga inicial

---
+++ 2Boperçãoaritimetica/script.js
@@ -0,0 +1,35 @@
+// var a = 78;
+// var a = 15;
+// var total = A + b;
+// console.log (total);
+
+// var a = 78;
+// var a = 15;
+// var total = A - b;
+// console.log (total);
+
+// var a = 78
+// var a = 15;
+// var total = A * b;
+// console.log (total);
+
+// var a = 78
+// var a = 15;
+// var total = A \ b;
+// console.log (total);
+
+// var a = 78
+// var a = 15;
+// var total = A % b;
+// console.log (total);
+// var a = 78
+// var a = 15;
+// var total = A ++ b;
+// console.log (total);
+
+// var a = 78
+// var a = 15;
+// var total = A -- b;
+// console.log (total);
+
+
